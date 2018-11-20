09f566ce729b9c44226bfd6e726aeb0a19771984
AbhishekGupta1506 <abhishekgupta1506@gmail.com>
2018-11-20 16:48:07 +0530

    updated the script to point ot current dir by using %cd%

--- script/sagabeinstall.bat
+++ script/sagabeinstall.bat
@@ -14,14 +14,14 @@ if exist %install.dir% (
 	echo "SoftwareAG%version%ABE installation exist at C:"
 	) else (
 		 echo "SoftwareAG%version%ABE installation does exist at C: preparing to install"
-		 if exist C:%homepath%\Downloads\SoftwareAGInstaller.jar (
+		 if exist %cd%\SoftwareAGInstaller.jar (
 		 echo "Installer exist"
 		 ) else (
 			   echo "downloading... SoftwareAG installer ...."
-			   curl -o C:%homepath%\Downloads\SoftwareAGInstaller.jar %installer%
+			   curl -o %cd%\SoftwareAGInstaller.jar %installer%
 			   )
 		echo "Installing ... ABE in directory C:\SoftwareAG%version%ABE ..."
-		start /B /wait java -jar C:%homepath%\Downloads\SoftwareAGInstaller.jar -readScript C:\Users\Administrator\Desktop\ABEInstallationScript.txt -console
+		start /B /wait java -jar %cd%\SoftwareAGInstaller.jar -readScript %cd%\ABEInstallationScript.txt -console
     )
 
 REM exit
